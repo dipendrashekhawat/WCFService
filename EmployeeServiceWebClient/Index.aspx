baed8ab1d4a4dd43be563c1f8cfa19bb1b3c1c58
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-07-18 16:21:23 +0530

    Creating Repository for WCF service

---
+++ EmployeeServiceWebClient/Index.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="EmployeeServiceWebClient.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="GetEmployeeData" runat="server" Text="Get Employee Data" OnClick="GetEmployeeData_Click" />
+        <br />
+        <asp:FileUpload ID="FileUpload1"  CssClass="button"  runat="server" />
+        <asp:Button ID="ImportCSV" runat="server" Text="Upload CSV File" OnClick="ImportCSV_Click" />
+        <br />
+        <h2>Employee Details</h2>
+        <br />
+        <b>FirstName:</b> <asp:Label ID="FirstName" runat="server" Text=""></asp:Label>
+        <br />
+        <b>LastName:</b> <asp:Label ID="LastName" runat="server" Text=""></asp:Label>
+        <br />
+        <b>Department:</b> <asp:Label ID="Department" runat="server" Text=""></asp:Label>
+        <br />
+        <b>Designation:</b> <asp:Label ID="Designation" runat="server" Text=""></asp:Label>
+        <br />
+        <b>Error Message:</b> <asp:Label ID="DisplayMessage" runat="server" Text=""></asp:Label>
+    </div>
+
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ EmployeeServiceWebClient/Index.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ServiceModel.Channels;
+using System.ServiceModel;
+using EmployeeServiceWebClient.EmployeeServiceRef;
+using System.IO;
+using System.Data;
+
+namespace EmployeeServiceWebClient
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GetEmployeeData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageHeader header;
+                OperationContextScope scope;
+                string userToken = string.Empty;
+
+                EmployeeServiceClient objEmployeeClient = new EmployeeServiceClient();
+
+                userToken = objEmployeeClient.Login("guest1", "ABC");
+
+                scope = new OperationContextScope(objEmployeeClient.InnerChannel);
+
+                header = MessageHeader.CreateHeader("TokenHeader", "TokenNameSpace", userToken);
+
+                OperationContext.Current.OutgoingMessageHeaders.Add(header);
+
+                EmployeeDetails objEmployee1 = new EmployeeDetails();
+                objEmployee1 = objEmployeeClient.GetMyEmployees();
+
+                //Displaying result
+                FirstName.Text = objEmployee1.FirstName;
+                LastName.Text = objEmployee1.LastName;
+                Department.Text = objEmployee1.Department;
+                Designation.Text = objEmployee1.Designation;
+                DisplayMessage.Text = "No Error";
+
+            }
+            catch (Exception ex)
+            {
+                DisplayMessage.Text = ex.Message;
+            }
+        }
+
+        protected void ImportCSV_Click(object sender, EventArgs e)
+        {
+            //Upload and save the file  
+            string csvPath = Server.MapPath("~/Files/") + Path.GetFileName(FileUpload1.PostedFile.FileName);
+            FileUpload1.SaveAs(csvPath);
+
+            //Create a DataTable.  
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[4] {   
+                                new DataColumn("FirstName", typeof(string)),  
+                                new DataColumn("LastName", typeof(string)),  
+                                new DataColumn("Department", typeof(string)),  
+                                new DataColumn("Designation",typeof(string)) 
+            });
+
+            //Read the contents of CSV file.  
+            string csvData = File.ReadAllText(csvPath);
+
+            //Execute a loop over the rows.  
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    dt.Rows.Add();
+                    int i = 0;
+
+                    //Execute a loop over the columns.  
+                    foreach (string cell in row.Split(','))
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell;
+                        i++;
+                    }
+                }
+            }
+
+            //Bind the DataTable.  
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+
+}
